cbbaae05c3f49adc06d5d4e56b4121e25925736c
blakebryan <117868509+blakebryan@users.noreply.github.com>
2022-11-17 15:54:11 -0600

    Update operator.js

--- operator.js
+++ operator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './operator.css';
 
 
 class Operator extends React.Component {
@@ -9,11 +10,11 @@ class Operator extends React.Component {
                 <h1 id="heading">Roommify</h1>
                 <h4>Operator View</h4>
                 <div className="col-12">
-                    <button type="button" className="btn btn-primary" onClick={() => this.props.nextPage(1)}>View Completion Progress</button>
-                    <button type="button" className="btn btn-primary">Create Pairings</button>
+                    <button type="button" className="btn btn-primary" id="opButtons" onClick={() => this.props.nextPage(1)}><p>View Completion Progress</p></button>
+                    <button type="button" className="btn btn-primary" id="opButtons"><p>Create Pairings</p></button>
                 </div>
                 <div className="col-12">
-                    <button type="button" className="btn btn-primary"><p>New Selection</p>(Complete current selection first)</button>
+                    <button type="button" className="btn btn-primary disabled" id="opButtons"><p>New Selection</p>(Complete current selection first)</button>
                 </div>
             </div>
         )
