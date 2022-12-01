b8c14eff1ab40d253b087ae30c05d6e0ea6e1822
blakebryan <117868509+blakebryan@users.noreply.github.com>
2022-11-30 18:18:41 -0600

    Update RoomieEmail.js

--- RoomieEmail.js
+++ RoomieEmail.js
@@ -2,15 +2,15 @@ import React from "react";
 import DB from './db.json';
 
 class RoomieEmail extends React.Component{
-    displayRoomies = () => { // eslint-disable-next-line
+    displayRoomies = () => {
         let i;
         for (i = 0; i < DB.accounts.length; i++) {
             if (DB.accounts[i].id === localStorage.getItem("userID")) {
                 break;
             }
-        }
+        } // eslint-disable-next-line
         const roomies = DB.accounts.map(el => {
-            if (Math.abs(el.score - DB.accounts[i].score <= 3) && el.id != DB.accounts[i].id){
+            if (Math.abs(el.score - DB.accounts[i].score <= 3) && el.id !== DB.accounts[i].id){
                 console.log(el.name);
                return (<li>{el.name}</li>)
             }
